21325e1ac8ea51444c258eb84a52e5f2be6df363
pedroceciliocn <pedroceciliocn@gmail.com>
2021-03-05 15:00:29 -0300

    chapter 14 started

--- game_stats.py
+++ game_stats.py
@@ -6,8 +6,8 @@ class GameStats:
 		self.settings = ai_game.settings
 		self.reset_status()
 
-		# Start Alien Invasion in an active state.
-		self.game_active = True
+		# Start Alien Invasion in an inactive state.
+		self.game_active = False
 
 	def reset_status(self):
 		"""Initialize statistics that can change during the game."""
