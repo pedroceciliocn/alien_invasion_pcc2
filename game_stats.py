4d34ac66e96bd2b4e71d079c50a7dba6a13ab51b
pedroceciliocn <pedroceciliocn@gmail.com>
2021-03-05 11:20:21 -0300

    Chapter 13 done

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+class GameStats:
+	"""Track statistics for Alien Invasion."""
+
+	def __init__(self, ai_game):
+		"""Initialize statistics."""
+		self.settings = ai_game.settings
+		self.reset_status()
+
+		# Start Alien Invasion in an active state.
+		self.game_active = True
+
+	def reset_status(self):
+		"""Initialize statistics that can change during the game."""
+		self.ships_left = self.settings.ship_limit
+
